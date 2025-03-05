d1b7ab835c7a4f29af5f8478d5e73d8d39505ad3
Joshua Kndel <joshua.kandel@porsche.de>
2025-03-05 13:22:35 +0100

    chore: format customErrors

--- source/map/customErrors.py
+++ source/map/customErrors.py
@@ -1,18 +1,17 @@
-
-
-class TilesNotNearbyError(Exception):
-    """
+"""
         Exception wenn Tiles nicht nebeneinander sind
-    """
-
+"""
+class TilesNotNearbyError(Exception):
+    
     def __init__(self):
         super().__init__("Tiles are not nearby!")
 
 
-class TilesCorrespondError(Exception):
-    """
+"""
         Exception wenn Tiles die selbe Position haben
-    """
+"""
+class TilesCorrespondError(Exception):
+    
 
     def __init__(self):
         super().__init__("Tiles have the same position!")
